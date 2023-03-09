5501b24c907e3f899e694638185a76d0a1bda231
GentMaker <daohung.rks@gmail.com>
2023-03-09 17:09:21 +0700

    update_controller

--- src/main/java/ProjectBlogOJT/model/repository/ProductRepository.java
+++ src/main/java/ProjectBlogOJT/model/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package ProjectBlogOJT.model.repository;
 
+import ProjectBlogOJT.model.entity.Product;
+import com.sun.tools.javac.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    List<Product> findProductByProductNameContaining(String productName);
+
 }
